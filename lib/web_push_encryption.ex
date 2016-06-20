0e3620677a12011b53843a59eef1d0b21f49facd
Francisco Lopera Cordoba <FcoLopera89@gmail.com>
2016-06-20 14:16:02 +0200

    Make web push encryption works with both, chrome and mozilla endpoints

--- lib/web_push_encryption.ex
+++ lib/web_push_encryption.ex
@@ -4,6 +4,7 @@ defmodule WebPushEncryption do
   """
 
   defdelegate send_web_push(message, subscription, auth_token), to: WebPushEncryption.Push
+  defdelegate send_web_push(message, subscription), to: WebPushEncryption.Push
 
   defdelegate encrypt(message, subscription), to: WebPushEncryption.Encrypt
   defdelegate encrypt(message, subscription, padding_length), to: WebPushEncryption.Encrypt
