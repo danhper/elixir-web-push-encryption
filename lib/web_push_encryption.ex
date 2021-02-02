01756dad22e081bb275947d353691de6c4ae7522
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2021-02-02 15:13:45 +0100

    add send_web_push/4 (with TTL)
    This is still missing on the API.

--- lib/web_push_encryption.ex
+++ lib/web_push_encryption.ex
@@ -3,6 +3,7 @@ defmodule WebPushEncryption do
   Facade module to access encryption and push functionalities
   """
 
+  defdelegate send_web_push(message, subscription, auth_token, ttl), to: WebPushEncryption.Push
   defdelegate send_web_push(message, subscription, auth_token), to: WebPushEncryption.Push
   defdelegate send_web_push(message, subscription), to: WebPushEncryption.Push
 
